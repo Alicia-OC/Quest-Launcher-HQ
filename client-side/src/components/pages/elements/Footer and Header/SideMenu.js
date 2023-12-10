2330d4e7cc67b2240d38b80eadd17769231db627
Alicia Olivares Canales <alicia93oc@gmail.com>
2023-12-10 21:14:09 +0100

    modified the SideMenu, now it shows a list of the templates. In the future Id like tyo add a favorite finctionality where the suer can star templates and the sidemenu will only show the starred templates

--- client-side/src/components/pages/elements/Footer and Header/SideMenu.js
+++ client-side/src/components/pages/elements/Footer and Header/SideMenu.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
+import GetTemplate from "../../../../features/templates/GetTemplate";
 
 function SideMenu() {
+  const templates = GetTemplate();
+
+  let templateTitlesArrtest;
+
+  if (templates) {
+    templateTitlesArrtest = templates.map((element) => (
+      <li id={element._id}>
+        <a className="" href={"/Template/" + element._id}>
+          {element.templateTitle.slice(0, 12)}...
+        </a>
+      </li>
+    ));
+  }
+
   return (
     <div>
       <p>This is a test</p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+
+      <ul> {templateTitlesArrtest}</ul>
     </div>
   );
 }
